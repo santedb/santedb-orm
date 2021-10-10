4b9b5aa20d460082774abed9cc1aad9144098b10
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-10 15:42:55 -0400

    Updated the merge set

--- SanteDB.OrmLite/IOrmResultSet.cs
+++ SanteDB.OrmLite/IOrmResultSet.cs
@@ -2,22 +2,24 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,13 +29,11 @@ using System.Threading.Tasks;
 
 namespace SanteDB.OrmLite
 {
-
     /// <summary>
     /// Non-generic interface
     /// </summary>
     public interface IOrmResultSet : IEnumerable
     {
-
         /// <summary>
         /// Gets the SQL statement that this result set is based on
         /// </summary>
@@ -112,7 +112,7 @@ namespace SanteDB.OrmLite
         bool Any();
 
         /// <summary>
-        /// Select the 
+        /// Select the
         /// </summary>
         /// <param name="property">The fields to be selected</param>
         OrmResultSet<TElement> Select<TElement>(string property);
