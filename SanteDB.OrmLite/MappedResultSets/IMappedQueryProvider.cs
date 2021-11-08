af67239723455d46eac475ec2a6acf7bf1fcaa9d
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-08 09:54:15 -0500

    Updated ORM to have common iterator query results

---
+++ SanteDB.OrmLite/MappedResultSets/IMappedQueryProvider.cs
@@ -0,0 +1,52 @@
+﻿using SanteDB.Core.Model;
+using SanteDB.Core.Services;
+using SanteDB.OrmLite;
+using SanteDB.OrmLite.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SanteDB.OrmLite.MappedResultSets
+{
+    /// <summary>
+    /// Represents an ADO query provider which is used by wrapped result set
+    /// instance to calculate its result set.
+    /// </summary>
+    public interface IMappedQueryProvider<TModel>
+    {
+        /// <summary>
+        /// Get the provider
+        /// </summary>
+        IDbProvider Provider { get; }
+
+        /// <summary>
+        /// Gets the instance of the query persistence service
+        /// </summary>
+        IQueryPersistenceService QueryPersistence { get; }
+
+        /// <summary>
+        /// Execute a query for the specified model with query identifier returning the results
+        /// </summary>
+        /// <param name="query">The query to execute</param>
+        /// <returns>The amount of results to return</returns>
+        IOrmResultSet ExecuteQueryOrm(DataContext context, Expression<Func<TModel, bool>> query);
+
+        /// <summary>
+        /// Get the specified object at the specified key
+        /// </summary>
+        TModel Get(DataContext context, Guid key);
+
+        /// <summary>
+        /// Convert <paramref name="result"/> to model
+        /// </summary>
+        TModel ToModelInstance(DataContext context, object result);
+
+        /// <summary>
+        /// Map sort expression
+        /// </summary>
+        /// <param name="sortExpression">The sort expression to map</param>
+        /// <returns>The mapped sort expression</returns>
+        Expression MapSortExpression(Expression<Func<TModel, dynamic>> sortExpression);
+    }
+}
