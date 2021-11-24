328982cacd7379f1b787623d87cac296f56ce6a3
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-24 10:28:02 -0500

    Updated to match new configuration interface switch rom Dictionary<> to IDictionary<>

--- SanteDB.OrmLite/Providers/Postgres/PostgreSQLConfigurationProvider.cs
+++ SanteDB.OrmLite/Providers/Postgres/PostgreSQLConfigurationProvider.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.Core;
 using SanteDB.Core.Configuration;
 using SanteDB.Core.Configuration.Data;
@@ -31,7 +32,6 @@ namespace SanteDB.OrmLite.Providers.Postgres
     /// </summary>
     public class PostgreSQLConfigurationProvider : AdoNetConfigurationProvider
     {
-
         /// <summary>
         /// Gets the invariant
         /// </summary>
@@ -50,7 +50,7 @@ namespace SanteDB.OrmLite.Providers.Postgres
         /// <summary>
         /// Get the options for connecting
         /// </summary>
-        public override Dictionary<string, ConfigurationOptionType> Options => new Dictionary<string, ConfigurationOptionType>(){
+        public override IDictionary<string, ConfigurationOptionType> Options => new Dictionary<string, ConfigurationOptionType>(){
             { "host", ConfigurationOptionType.String },
             { "port", ConfigurationOptionType.Numeric },
             { "user id", ConfigurationOptionType.User },
@@ -64,7 +64,7 @@ namespace SanteDB.OrmLite.Providers.Postgres
         /// <summary>
         /// Option groups
         /// </summary>
-        public override Dictionary<String, String[]> OptionGroups => new Dictionary<string, string[]>()
+        public override IDictionary<String, String[]> OptionGroups => new Dictionary<string, string[]>()
         {
             { "Connection", new string[]{ "host","port","user id","password","database"} },
             { "Pooling", new string[] { "pooling", "minpoolsize", "maxpoolsize" } }
@@ -180,8 +180,6 @@ namespace SanteDB.OrmLite.Providers.Postgres
             }
 
             return connectionString;
-
-
         }
     }
 }
