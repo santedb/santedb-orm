5081bfc1c9ca3cc310874f686002987064eabad4
Justin Fyfe <justin@fyfesoftware.ca>
2024-06-10 10:55:14 -0400

    JIMS-301 JIMS-304 Updates the configuration manager so that the owner and dedicated facility in the security settings is a single option\

---
+++ SanteDB.OrmLite/Providers/IDbBulkProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.OrmLite.Providers
+{
+    /// <summary>
+    /// Represents a bulk data provider which can read/write from memory for faster inserts
+    /// </summary>
+    public interface IDbBulkProvider : IDbProvider
+    {
+
+        /// <summary>
+        /// Gets a memory connection or temporary connection where data can be inserted quickly
+        /// </summary>
+        /// <returns>The memory data connection</returns>
+        DataContext GetBulkConnection();
+
+        /// <summary>
+        /// Flushes the contents of <paramref name="bulkContext"/> into a connection obtained via <see cref="IDbProvider.GetWriteConnection"/>
+        /// </summary>
+        /// <param name="bulkContext">The data context opened by <see cref="GetBulkConnection"/> which is to be flushed</param>
+        void FlushBulkConnection(DataContext bulkContext);
+
+    }
+}
