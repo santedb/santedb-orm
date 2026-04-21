8cdbf3fce76946fbade15c4ed93c5ecd0ee5db96
Justin Fyfe <justin@fyfesoftware.ca>
2026-04-21 18:34:54 -0400

    Adds the postgresql replication role

---
+++ SanteDB.OrmLite/Providers/Postgres/PostgreSQLReplicationRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.OrmLite.Providers.Postgres
+{
+    /// <summary>
+    /// Replication mode
+    /// </summary>
+    internal enum PostgreSQLReplicationRole
+    {
+        origin,
+        replica
+    }
+}
